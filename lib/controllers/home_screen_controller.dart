4ea6e1a919636a84990d7e7b20c70e90be4ecdba
Ishaan Jindal <ishaanjindal2006@gmail.com>
2025-10-23 11:42:01 +0530

    added welcome note

--- lib/controllers/home_screen_controller.dart
+++ lib/controllers/home_screen_controller.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:showcaseview/showcaseview.dart';
+import 'package:writer/controllers/note_controller.dart';
+import 'package:writer/data/models/note.dart';
 
 class HomeScreenController extends GetxController {
 
@@ -33,7 +35,6 @@ class HomeScreenController extends GetxController {
         _onShowcaseFinish();
       },
       blurValue: 1.0,
-      autoPlay: true,
       autoPlayDelay: const Duration(seconds: 3),
       enableAutoScroll: true,
       scrollDuration: Duration(milliseconds: 500),
@@ -54,11 +55,43 @@ class HomeScreenController extends GetxController {
     );
   }
 
+  void _createWelcomeNote() {
+    final noteController = Get.find<NoteController>();
+    final welcomeNote = Note(
+      title: 'Welcome.md',
+      content: """
+# Welcome to SwiftWrite!
+
+This is a simple note to get you started.
+
+## Features
+- **Markdown Support**: Write in Markdown and see it rendered.
+- **Code Highlighting**: Write code blocks and see them highlighted.
+- **File Support**: Open and save files in different formats.
+- **Tags**: Organize your notes with tags.
+- **Themes**: Switch between light, dark and fall themes.
+
+## Getting Started
+1. Create a new note by tapping the '+' button.
+2. Write your note in Markdown.
+3. Add tags to your note.
+4. Save your note.
+
+### Enjoy!
+""",
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+      tags: ['welcome', 'guide'],
+    );
+    noteController.addNote(welcomeNote);
+  }
+
   void _checkAndShowTutorial() {
     final settingsBox = Hive.box('settings');
     bool hasCompletedOnboarding = settingsBox.get('hasCompletedOnboarding', defaultValue: false);
 
     if (!hasCompletedOnboarding) {
+      _createWelcomeNote();
       Future.delayed(const Duration(milliseconds: 200), () {
         ShowcaseView.get().startShowCase(
           [
